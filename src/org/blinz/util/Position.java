b0afe2f72cb0634a20e5c6fe9c6378bf55e02815
Gary Talent <gtalent2@gmail.com>
2009-08-31 10:02:51 -0500

    Documented Position.

--- src/org/blinz/util/Position.java
+++ src/org/blinz/util/Position.java
@@ -17,55 +17,107 @@
 package org.blinz.util;
 
 /**
- *
- * @author gary
+ * Class for holding coordinates.
+ * @author Blinz Project
  */
 public class Position {
 
-    public int x = 0,  y = 0;
+    /**
+     * X coordinate of this Position.
+     */
+    public int x = 0;
+    /**
+     * Y coordinate of this Position.
+     */
+    public int y = 0;
 
+    /**
+     * Default constructer without parameters, location defaults to (0, 0).
+     */
     public Position() {
     }
 
+    /**
+     * Creates a new Position with the given coordinates.
+     * @param x
+     * @param y
+     */
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Creates a new Position with the values of position.
+     * @param position
+     */
     public Position(Position position) {
         x = position.x;
         y = position.y;
     }
 
+    /**
+     * Sets this Positions coordinate to that of the given coordinates.
+     * @param x
+     * @param y
+     */
     public final void setPosition(int x, int y) {
         setX(x);
         setY(y);
     }
 
+    /**
+     * Sets this Positions coordinates to that of the given Position.
+     * @param position
+     */
     public final void setPosition(Position position) {
         setPosition(position.x, position.y);
     }
 
+    /**
+     * Sets the x coordinate of this Position to the given value.
+     * @param x
+     */
     public final void setX(int x) {
         this.x = x;
     }
 
+    /**
+     * Sets the y coordinate of this Position to the given value.
+     * @param y
+     */
     public final void setY(int y) {
         this.y = y;
     }
 
+    /**
+     * Gets the x coordinate of this Position.
+     * @return x
+     */
     public final int getX() {
         return x;
     }
 
+    /**
+     * Gets the y coordinate of this Position.
+     * @return y
+     */
     public final int getY() {
         return y;
     }
 
+    /**
+     * Adds mod to this Position's x coordinate.
+     * @param mod
+     */
     public final void modX(int mod) {
         x += mod;
     }
 
+    /**
+     * Adds mod to this Position's y coordinate.
+     * @param mod
+     */
     public final void modY(int mod) {
         y += mod;
     }
