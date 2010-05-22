be3fe1f7ff9c3e4fd43afafe0c8c8b7657fdd076
Gary@WIN-7OFOHM21OOJ <Gary@WIN-7OFOHM21OOJ>
2010-05-21 19:14:29 -0500

    Undeprecated the org.blinz.util.concurrency package.

--- src/main/java/org/blinz/util/concurrency/SynchronizedTask.java
+++ src/main/java/org/blinz/util/concurrency/SynchronizedTask.java
@@ -20,7 +20,6 @@ package org.blinz.util.concurrency;
  * A non-re-entrant task. To be used on Tasks that need be executed once per loop.
  * @author Blinz
  */
-@Deprecated
 public abstract class SynchronizedTask extends Task {
 
     @Override
--- src/main/java/org/blinz/util/concurrency/TaskExecuter.java
+++ src/main/java/org/blinz/util/concurrency/TaskExecuter.java
@@ -21,7 +21,6 @@ package org.blinz.util.concurrency;
  * the extend.
  * @author Blinz
  */
-@Deprecated
 public final class TaskExecuter extends ParallelProcess {
 
     private TaskList list = new TaskList();
